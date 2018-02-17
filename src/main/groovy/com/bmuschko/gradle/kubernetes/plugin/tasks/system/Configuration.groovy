6c0d072aaf0a785e2eefd3ad68351792ee2f0370
dancc <Chris.Dancy@pega.com>
2018-02-17 16:54:57 -0500

    Added task 'Configuration' which prints out minimal system specs and returns the Configuration object.

---
+++ src/main/groovy/com/bmuschko/gradle/kubernetes/plugin/tasks/system/Configuration.groovy
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bmuschko.gradle.kubernetes.plugin.tasks.system
+
+import com.bmuschko.gradle.kubernetes.plugin.tasks.AbstractKubernetesTask
+
+/**
+ * Get, and print minimally, the system Configuration.
+ */
+class Configuration extends AbstractKubernetesTask {
+
+    @Override
+    def runRemoteCommand(kubernetesClient) {
+
+        final def configuration = kubernetesClient.getConfiguration()
+        logger.quiet "Api-Version: ${configuration.getApiVersion()}"
+        logger.quiet "Master-URL: ${configuration.getMasterUrl()}"
+
+        // register response for downstream use and return list of items
+        // for `onNext` execution.
+        registerResponse(configuration)
+    }
+}
+
