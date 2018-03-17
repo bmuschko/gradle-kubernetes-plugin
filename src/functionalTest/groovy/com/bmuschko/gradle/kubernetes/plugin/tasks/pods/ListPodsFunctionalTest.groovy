ad3b9d454e98f9b78fb8b4f46d025982bec81e6d
dancc <Chris.Dancy@pega.com>
2018-03-17 10:48:37 -0400

    Added functional test for ListPods task.

---
+++ src/functionalTest/groovy/com/bmuschko/gradle/kubernetes/plugin/tasks/pods/ListPodsFunctionalTest.groovy
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bmuschko.gradle.kubernetes.plugin.tasks.services
+
+import com.bmuschko.gradle.kubernetes.plugin.AbstractFunctionalTest
+import org.gradle.testkit.runner.BuildResult
+
+/**
+ *
+ * All functional tests for the `ListPods` task.
+ *
+ */
+class ListPodsFunctionalTest extends AbstractFunctionalTest {
+/*
+    def "List pods, execute reactive-streams, with no config"() {
+        buildFile << """
+            import com.bmuschko.gradle.kubernetes.plugin.tasks.pods.ListPods
+
+            task listPods(type: ListPods) {
+                onError { exc ->
+                    logger.quiet "$ON_ERROR_NOT_REACHED exception=\${exc}"
+                }
+                onNext { output ->
+                    if (output) {
+                        logger.quiet "$ON_NEXT_REACHED with name \${output.getMetadata().getName()}"
+                    }
+                }
+                onComplete {
+                    logger.quiet '$ON_COMPLETE_REACHED'
+                }
+                doLast {
+                    if (response()) {
+                        logger.quiet '$RESPONSE_SET_MESSAGE'
+                    }
+                }
+            }
+
+            task workflow(dependsOn: listPods)
+        """
+
+        when:
+            BuildResult result = build('workflow')
+
+        then:
+            result.output.contains('Listing pods...')
+            !result.output.contains(ON_ERROR_NOT_REACHED)
+            result.output.contains(ON_NEXT_REACHED)
+            result.output.contains(ON_COMPLETE_REACHED)
+            result.output.contains(RESPONSE_SET_MESSAGE)
+    }
+
+    def "List pods, execute reactive-streams, and with config"() {
+        buildFile << """
+            import com.bmuschko.gradle.kubernetes.plugin.tasks.pods.ListPods
+
+            task listPods(type: ListPods) {
+                config {
+                    withLabel("${randomString()}")
+                }
+                onError { exc ->
+                    logger.quiet "$ON_ERROR_NOT_REACHED: exception=\${exc}"
+                }
+                onNext { output ->
+                    logger.quiet '$SHOULD_NOT_REACH_HERE'
+                }
+                onComplete {
+                    logger.quiet '$ON_COMPLETE_REACHED'
+                }
+                doLast {
+                    if (response()) {
+                        logger.quiet '$RESPONSE_SET_MESSAGE'
+                    }
+                }
+            }
+
+            task workflow(dependsOn: listPods)
+        """
+
+        when:
+            BuildResult result = build('workflow')
+
+        then:
+            result.output.contains('Listing pods...')
+            !result.output.contains(ON_ERROR_NOT_REACHED)
+            !result.output.contains(SHOULD_NOT_REACH_HERE)
+            result.output.contains(ON_COMPLETE_REACHED)
+            result.output.contains(RESPONSE_SET_MESSAGE)
+    }
+*/
+    def "List non-existent pods with config"() {
+        buildFile << """
+            import com.bmuschko.gradle.kubernetes.plugin.tasks.pods.ListPods
+
+            task listPods(type: ListPods) {
+                config {
+                    withLabels(["${randomString()}" : "${randomString()}"])
+                    withoutLabels(["${randomString()}" : "${randomString()}"])
+                }
+
+                onError { exc ->
+                    logger.quiet "$ON_ERROR_NOT_REACHED: exception=\${exc}"
+                }
+                onNext { output ->
+                    logger.quiet '$SHOULD_NOT_REACH_HERE'
+                }
+                onComplete {
+                    logger.quiet '$ON_COMPLETE_REACHED'
+                }
+                doLast {
+                    if (response()) {
+                        logger.quiet '$RESPONSE_SET_MESSAGE'
+                    }
+                }
+            }
+
+            task workflow(dependsOn: listPods)
+        """
+
+        when:
+            BuildResult result = build('workflow')
+
+        then:
+            result.output.contains('Listing pods...')
+            !result.output.contains(ON_ERROR_NOT_REACHED)
+            !result.output.contains(SHOULD_NOT_REACH_HERE)
+            result.output.contains(ON_COMPLETE_REACHED)
+            result.output.contains(RESPONSE_SET_MESSAGE)
+    }
+
+    def "List non-existent pods"() {
+        buildFile << """
+            import com.bmuschko.gradle.kubernetes.plugin.tasks.pods.ListPods
+
+            task listPods(type: ListPods) {
+                withLabels = ["${randomString()}" : "${randomString()}"]
+                withoutLabels = ["${randomString()}" : "${randomString()}"]
+
+                onError { exc ->
+                    logger.quiet "$ON_ERROR_NOT_REACHED: exception=\${exc}"
+                }
+                onNext { output ->
+                    logger.quiet '$SHOULD_NOT_REACH_HERE'
+                }
+                onComplete {
+                    logger.quiet '$ON_COMPLETE_REACHED'
+                }
+                doLast {
+                    if (response()) {
+                        logger.quiet '$RESPONSE_SET_MESSAGE'
+                    }
+                }
+            }
+
+            task workflow(dependsOn: listPods)
+        """
+
+        when:
+            BuildResult result = build('workflow')
+
+        then:
+            result.output.contains('Listing pods...')
+            !result.output.contains(ON_ERROR_NOT_REACHED)
+            !result.output.contains(SHOULD_NOT_REACH_HERE)
+            result.output.contains(ON_COMPLETE_REACHED)
+            result.output.contains(RESPONSE_SET_MESSAGE)
+    }
+}
