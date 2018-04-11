715076b628b8e656f390d4c5340db962c74f25e1
dancc <Chris.Dancy@pega.com>
2018-04-11 14:36:43 -0400

    Bump kubernetes to 1.9.4 and force use of localkube due to known regression with minikube.

--- scripts/mini-kube.sh
+++ scripts/mini-kube.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 export CHANGE_MINIKUBE_NONE_USER=true
-KUBE_VERSION="v1.9.0"
+KUBE_VERSION="v1.9.4"
 
 # Download kubectl, which is a requirement for using minikube.
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$KUBE_VERSION/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 
 # Download minikube and start.
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-sudo minikube start --vm-driver=none --kubernetes-version=$KUBE_VERSION
+sudo minikube start --vm-driver=none --bootstrapper=localkube --kubernetes-version=$KUBE_VERSION
 
 # Fix the kubectl context, as it's often stale.
 sudo minikube update-context
