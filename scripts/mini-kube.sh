62f6d04ae12f8ece5981cecafeb065c0cababd7d
Christopher Dancy <cdancy@users.noreply.github.com>
2018-10-04 16:01:53 -0400

    Update mini-kube.sh

--- scripts/mini-kube.sh
+++ scripts/mini-kube.sh
@@ -8,6 +8,8 @@ curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$KUBE
 
 # Download minikube and start.
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+sudo minikube delete
 sudo minikube start --vm-driver=none --bootstrapper=localkube --kubernetes-version=$KUBE_VERSION
 
 # Fix the kubectl context, as it's often stale.
