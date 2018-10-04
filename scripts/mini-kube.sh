d83290d071db8e92b8fd16c52faed3de6e6a8968
Christopher Dancy <cdancy@users.noreply.github.com>
2018-10-04 16:18:26 -0400

    Update mini-kube.sh

--- scripts/mini-kube.sh
+++ scripts/mini-kube.sh
@@ -3,7 +3,6 @@
 sudo mount --make-rshared /
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-sudo minikube start --vm-driver=none --bootstrapper=localkube --kubernetes-version=v1.10.0
-sudo minikube update-context
+sudo minikube start --vm-driver=none --kubernetes-version=v1.10.0
 JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
 kubectl cluster-info
