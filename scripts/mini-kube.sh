81c61acefd983d2a7c938250e0950d8f1eb9a316
Christopher Dancy <cdancy@users.noreply.github.com>
2018-10-15 16:46:22 -0400

    Update mini-kube.sh

--- scripts/mini-kube.sh
+++ scripts/mini-kube.sh
@@ -1,17 +1,84 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-mkdir -p ~/.minikube
-mkdir -p ~/.kube
-rm -rf ~/.minikube/*
-rm -rf ~/.kube/*
+################################################################################
+################################################################################
+################################################################################
+#
+# Start (via up) or Stop (via down) a minikube/kubernetes instance.
+#
+# If minikube/kubernetes is not installed than an attempt will be made to
+# install it using docker containers.
+#
+################################################################################
+################################################################################
+################################################################################
+
+# so we can execute this script from any location
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$PARENT_PATH/../"
+
+# create minikube dirs it not already present
+mkdir -p $HOME/.kube
+mkdir -p $HOME/.minikube
 touch $HOME/.kube/config
 
-export MINIKUBE_HOME=$HOME
-export CHANGE_MINIKUBE_NONE_USER=true
+# check, and potentially create with sensible defaults, the necessary env-vars
+export MINIKUBE_WANTUPDATENOTIFICATION=${MINIKUBE_WANTUPDATENOTIFICATION:="false"}
+export MINIKUBE_WANTREPORTERRORPROMPT=${MINIKUBE_WANTREPORTERRORPROMPT:="false"}
+export MINIKUBE_HOME=${MINIKUBE_HOME:="$HOME"}
+export CHANGE_MINIKUBE_NONE_USER=${CHANGE_MINIKUBE_NONE_USER:="true"}
+export KUBECONFIG=${KUBECONFIG:="$HOME/.kube/config"}
 
-sudo mount --make-rshared /
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-sudo minikube start --vm-driver=none --kubernetes-version=v1.10.0
-JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
-kubectl cluster-info
+# stand up minikube/kubernetes instance
+up () {
+
+  # install minikube if not already present
+  exit_status=`command -v minikube &> /dev/null; echo $?`
+  if [ $exit_status -eq 1 ]; then
+    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    chmod +x minikube
+    sudo cp minikube /usr/local/bin/
+    rm minikube
+  fi
+
+  # install kubectl if not already present
+  exit_status=`command -v kubectl &> /dev/null; echo $?`
+  if [ $exit_status -eq 1 ]; then
+    curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+    chmod +x kubectl
+    sudo cp kubectl /usr/local/bin/
+    rm kubectl
+  fi
+
+  # if not already started attempt to start
+  exit_status=`sudo minikube status &> /dev/null; echo $?`
+  if [ $exit_status -eq 1 ]; then
+    sudo -E minikube start --vm-driver=none
+
+    # this for loop waits until kubectl can access the api server that Minikube has created
+    for i in {1..150}; do # timeout for 5 minutes
+       exit_status=`kubectl get po &> /dev/null; echo $?`
+       if [ $exit_status -ne 1 ]; then
+          break
+      fi
+      sleep 2
+    done
+  else
+    echo minikube is already running...
+  fi
+}
+
+# stop down minkube/kubernetes instance
+down () {
+  exit_status=`command -v minikube &> /dev/null; echo $?`
+  if [ $exit_status -eq 0 ]; then sudo minikube stop
+  fi
+  exit 0
+}
+
+if [ "$1" == "up" ]; then up
+elif [ "$1" == "down" ]; then down
+else
+   echo "Must supply either [up|down] command"
+   exit 0
+fi
